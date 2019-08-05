178eeec478d62fe5acecaa64781a714ab304d17d
Michael Bowman <michaelcbowman@outlook.com>
2019-08-05 13:48:14 -0400

    Add project files.

---
+++ OurSchedule/OurSchedule/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using OurSchedule.Models;
+
+namespace OurSchedule.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
